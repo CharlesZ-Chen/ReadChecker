607efbf4fb8286933be15b6c2efdfb651d4a61e4
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-05-15 23:04:29 -0400

    first version of build.xml, and also with travis-build scripts

---
+++ .travis-build-without-test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+ROOT=$TRAVIS_BUILD_DIR/..
+
+# Fail the whole script if any command fails
+set -e
+
+export SHELLOPTS
+
+export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
+
+export JSR308=$ROOT
+export AFU=$ROOT/annotation-tools/annotation-file-utilities
+export CHECKERFRAMEWORK=$ROOT/checker-framework
+
+export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
+
+## Build Checker Framework
+if [ -d $ROOT/checker-framework ] ; then
+    # Older versions of git don't support the -C command-line option
+    (cd $ROOT/checker-framework && git pull)
+else
+    (cd $ROOT && git clone --depth 1 https://github.com/pascaliUWat/checker-framework.git)
+fi
+
+# This also builds annotation-tools and jsr308-langtools
+(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
+
+## Build plume-lib
+if [ -d $ROOT/plume-lib ] ; then
+    # Older versions of git don't support the -C command-line option
+    (cd $ROOT/plume-lib && git pull)
+else
+    (cd $ROOT && git clone --depth 1 https://github.com/mernst/plume-lib.git)
+fi
+
+(cd $ROOT/plume-lib/ && make)
+
+#build this checker
+ant -f $TRAVIS_BUILD_DIR/build.xml
---
+++ .travis-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ROOT=$TRAVIS_BUILD_DIR/..
+
+# Fail the whole script if any command fails
+set -e
+
+export SHELLOPTS
+
+. ./.travis-build-without-test.sh
+
+ant -f $TRAVIS_BUILD_DIR/tests.xml run-tests
