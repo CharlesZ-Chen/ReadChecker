412fc49c85bf4c747d0b8841eaeb5136757c3dd3
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-05-15 23:06:34 -0400

    using feild to store type qualifers e,g, SAFETY_BOTTOM

--- src/read/ReadAnnotatedTypeFactory.java
+++ src/read/ReadAnnotatedTypeFactory.java
@@ -1,8 +1,11 @@
 package read;
 
+import javax.lang.model.element.AnnotationMirror;
+
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.javacutil.AnnotationUtils;
 
 import read.qual.SafetyBottom;
 import read.qual.UnknownSafety;
@@ -11,15 +14,16 @@ import com.sun.source.tree.LiteralTree;
 import com.sun.source.tree.Tree;
 
 public class ReadAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
+    protected AnnotationMirror SAFETY_BOTTOM;
 
     public ReadAnnotatedTypeFactory(BaseTypeChecker checker) {
         super(checker);
         this.postInit();
+        SAFETY_BOTTOM = AnnotationUtils.fromClass(elements, SafetyBottom.class);
     }
 
     @Override
     protected void annotateImplicit(Tree tree, AnnotatedTypeMirror type, boolean iUseFlow) {
-        super.annotateImplicit(tree, type, iUseFlow);
         if (tree instanceof LiteralTree) {
             LiteralTree literalTree = (LiteralTree) tree;
             if (literalTree.getKind() == Tree.Kind.INT_LITERAL) {
@@ -31,7 +35,7 @@ public class ReadAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
                 }
             }
         }
-        
+        super.annotateImplicit(tree, type, iUseFlow);
     }
 
 }
