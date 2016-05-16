85a2deeb77cdc60cb560671b61ebc908e8cf49ab
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-05-15 23:04:59 -0400

    refine tests file structure

---
+++ tests/src/ReadTest.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+import org.checkerframework.framework.test.CheckerFrameworkTest;
+import org.junit.runners.Parameterized.Parameters;
+
+public class ReadTest extends CheckerFrameworkTest {
+
+    public ReadTest(File testFile) {
+        super(testFile, 
+                read.ReadChecker.class,
+                "",
+                "-Anomsgtext");
+    }
+
+    @Parameters
+    public static String[] getTestDirs() {
+        return new String[]{"read-typeHierarchy"};
+    }
+
+
+}
