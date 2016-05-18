50c8930c0943c539c8a52e6672035b81df674ef7
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-05-17 22:11:17 -0400

    patch of the base version of flow sensitive analysis. Changes in RAT should come with the previous commit

--- src/read/ReadAnnotatedTypeFactory.java
+++ src/read/ReadAnnotatedTypeFactory.java
@@ -1,20 +1,31 @@
 package read;
 
-import javax.lang.model.element.AnnotationMirror;
+import java.util.List;
 
-import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.VariableElement;
+
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.framework.flow.CFStore;
+import org.checkerframework.framework.flow.CFValue;
+import org.checkerframework.framework.type.GenericAnnotatedTypeFactory;
 import org.checkerframework.javacutil.AnnotationUtils;
+import org.checkerframework.javacutil.Pair;
 
 import read.qual.SafetyBottom;
 
-public class ReadAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
+public class ReadAnnotatedTypeFactory extends GenericAnnotatedTypeFactory<CFValue, CFStore, ReadTransfer, ReadAnalysis> {
     protected AnnotationMirror SAFETY_BOTTOM;
 
     public ReadAnnotatedTypeFactory(BaseTypeChecker checker) {
         super(checker);
         this.postInit();
         SAFETY_BOTTOM = AnnotationUtils.fromClass(elements, SafetyBottom.class);
+    }
+
+    @Override
+    protected ReadAnalysis createFlowAnalysis(List<Pair<VariableElement, CFValue>> fieldValues) {
+        return new ReadAnalysis(checker, this, fieldValues);
     }
 
 }
