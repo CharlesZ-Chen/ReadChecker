fadc776e8dd7c8a88852e5329ed19dece2a3c71f
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-05-25 11:30:45 -0400

    make doc clearer

--- run-dljc.sh
+++ run-dljc.sh
@@ -2,10 +2,9 @@
 
 # this script depends on the checker branch of Charles's fork of dljc:
 # https://github.com/CharlesZ-Chen/do-like-javac.git
-
-# TODO: this is not a good way to solve the problem, but currently since
+# (this is not a good way to solve the problem, but currently since
 # dljc override the javac classpath in its cmd, thus I have to add a little code
-# of joining the sys CLASSPATH to the --classpath in dljc's javac cmd.
+# of joining the sys CLASSPATH to the --classpath in dljc's javac cmd.)
 
 ROOT=$(cd $(dirname "$0") && pwd)
 DLJC=$ROOT/../do-like-javac
