7bda875d2fa588a02222021da008ce8d45805ba5
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-06-02 20:14:40 -0400

    add building denpendences before running test

--- case-study/apache-tomee/tomee-SimpleJSonParser-bug.sh
+++ case-study/apache-tomee/tomee-SimpleJSonParser-bug.sh
@@ -4,6 +4,7 @@ cd $(dirname "$0")
 
 export ROOT=$(pwd)
 
+CONTAINER=$ROOT/tomee/container
 OPENEJB_CORE=$ROOT/tomee/container/openejb-core
 TEST_FILE_DIR=$ROOT/tomee/container/openejb-core/src/test/java/org/apache/openejb/config/
 TEST_FILE=$ROOT/ServiceClasspathReadCheckerTest.java
@@ -17,6 +18,11 @@ cd $TEST_FILE_DIR
 if [ ! -f ServiceClasspathReadCheckerTest.java ]; then
     ln -s $TEST_FILE ./
 fi
+
+## Build tomee/container first to get required dependences for testing
+cd $CONTAINER
+## quick build cmd: https://github.com/apache/tomee
+mvn -Pquick -Dsurefire.useFile=false -DdisableXmlReport=true -DuniqueVersion=false -ff -Dassemble -DskipTests -DfailIfNoTests=false clean install
 
 cd $OPENEJB_CORE
 
