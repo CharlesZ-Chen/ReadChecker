dc52b2ff645f128691e79ffe381a0da8fb987b3e
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-06-27 12:01:23 -0400

    make the report script more self-contained

--- case-study/apache-tomee/tomee-SimpleJSonParser-bug.sh
+++ case-study/apache-tomee/tomee-SimpleJSonParser-bug.sh
@@ -26,4 +26,15 @@ mvn -Pquick -Dsurefire.useFile=false -DdisableXmlReport=true -DuniqueVersion=fal
 
 cd $OPENEJB_CORE
 
-mvn -Dtest=**/ServiceClasspathReadCheckerTest.java test
+gtimeout 30 mvn -Dtest=**/ServiceClasspathReadCheckerTest.java test
+
+echo "---------------------------------------------------------------------------------------------"
+echo " R E P O R T"
+echo "---------------------------------------------------------------------------------------------"
+echo "Above test will stuck in infinite loop forever, thus I terminate it after it runs 5 minutes.
+The reason of this infinite loop is caused by the defect on line 53 in SimpleJSonParser.java:
+https://github.com/apache/tomee/blob/master/container/openejb-core/src/main/java/org/apache/openejb/util/SimpleJSonParser.java#L53
+In this file, Line 35 correctly reads a character from the input stream and line 36 comments that the integer comparison must precede the cast.
+However, lines 53 compares the character with -1, which will always fail.
+The same defects also appear on line 70 and line 89 in the same file."
+
