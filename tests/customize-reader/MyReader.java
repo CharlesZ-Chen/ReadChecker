9f3b76f2ddc7723e5170b13a5b636fb492637da7
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-11-13 15:29:51 -0500

    add test of customize reader

---
+++ tests/customize-reader/MyReader.java
@@ -0,0 +1,28 @@
+package read;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import read.qual.EnsureSafeIf;
+import read.qual.UnsafeRead;
+
+public class MyReader {
+    InputStream reader;
+
+    MyReader(InputStream reader) {
+        this.reader = reader;
+    }
+
+    public @UnsafeRead int read() {
+        int inbuff;
+        try {
+            inbuff = this.reader.read();
+        } catch (IOException e) {
+            inbuff = -1;
+        }
+        return inbuff;
+    }
+}
--- tests/src/ReadTest.java
+++ tests/src/ReadTest.java
@@ -15,7 +15,7 @@ public class ReadTest extends CheckerFrameworkPerDirectoryTest {
 
     @Parameters
     public static String[] getTestDirs() {
-        return new String[]{"read-typeHierarchy", "post-condition", "binaryOpRefine", "noc-examples",
+        return new String[]{"read-typeHierarchy", "post-condition", "binaryOpRefine", "customize-reader", "noc-examples",
                 "cs-examples", "casting", "teamed-quiz", "wtfCodeSOD061102"};
     }
 }
